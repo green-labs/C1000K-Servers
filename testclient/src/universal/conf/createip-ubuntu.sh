7a075a532f0aed33ae65a38fb97913d3c19a7f19
wickedev <orange881217@gmail.com>
2022-12-22 22:57:29 +0900

    createip for ubuntu script

---
+++ testclient/src/universal/conf/createip-ubuntu.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+ip addr add 172.22.100.151/24 dev ens5:1
+ip addr add 172.22.100.116/24 dev ens5:2
+ip addr add 172.22.100.53/24 dev ens5:3
+ip addr add 172.22.100.19 dev ens5:4
+ip addr add 172.22.100.48 dev ens5:5
+ip addr add 172.22.100.95 dev ens5:6
+ip addr add 172.22.100.125 dev ens5:7
+ip addr add 172.22.100.249 dev ens5:8
+ip addr add 172.22.100.57 dev ens5:9
+ip addr add 172.22.100.134 dev ens5:10
+ip addr add 172.22.100.103 dev ens5:11
+ip addr add 172.22.100.135 dev ens5:12
+ip addr add 172.22.100.71 dev ens5:13
+ip addr add 172.22.100.132 dev ens5:14
+ip addr add 172.22.100.228 dev ens5:15
+ip addr add 172.22.100.197 dev ens5:16
+ip addr add 172.22.100.78 dev ens5:17
+ip addr add 172.22.100.173 dev ens5:18
+ip addr add 172.22.100.13 dev ens5:19
+ip addr add 172.22.100.41 dev ens5:20
